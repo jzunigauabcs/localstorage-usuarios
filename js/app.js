7ccfec35e89de3e853eb15b8b884134290967dd2
Jesús Antonio Zúñiga Arce <jzuniga@uabcs.mx>
2024-10-23 02:39:05 -0700

    update

--- js/app.js
+++ js/app.js
@@ -1,24 +1,52 @@
-class LocalStorage {
+class DB {
     constructor() {
         this.key = 'usuarios';
-        this.usuarios = this.get() || [];
+        this.usuarios = this.get(this.key);
     }
 
-    store(usuario) {
-        this.set(usuario);
+    store(data) {
+        this.usuarios.push(data);
+        this.set(this.usuarios, this.key);
     }
 
     findAll() {
-        return this.get();
+        return this.get(this.key);
     }
 
-    get() {
-        return JSON.parse(localStorage.getItem(this.key));
+    get(key) {
+        return JSON.parse(localStorage.getItem(key)) || [];
     }
 
-    set(data) {
-        localStorage.setItem(this.key, JSON.stringify(data));
+    set(data, key) {
+        localStorage.setItem(key, JSON.stringify(data));
     }
+}
+
+function renderTable() {
+    const table = document.querySelector('.tbl-usuarios tbody');
+    table.innerHTML = '';
+
+    const db = new DB();
+
+    const usuarios = db.findAll();
+
+    usuarios.forEach((u) => {
+        const tr = document.createElement('tr');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        const td3 = document.createElement('td');
+
+        td1.appendChild(document.createTextNode(u.usuario));
+        td2.appendChild(document.createTextNode(u.email));
+        td3.appendChild(document.createTextNode(u.pais));
+        
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        table.appendChild(tr);
+    })
+
 }
 
 function guardarUsuario(e) {
@@ -34,13 +62,15 @@ function guardarUsuario(e) {
         email: tbxEmail.value,
         pais: cbxPais.value
     };
-    const local = new LocalStorage();
-    local.store(usuario);
+    const db = new DB();
+    db.store(usuario);
+    renderTable();
 }
 
 function init() {
     const btnGuardar = document.querySelector('#btnGuardar');
     btnGuardar.addEventListener('click', guardarUsuario);
+    renderTable();
 }
 
 document.addEventListener('DOMContentLoaded', init);
